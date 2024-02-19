178c1079e96b44b9e2ae3cd6d8a56bb673d5ee3e
vanhung88 <88levanhung@gmail.com>
2024-02-19 16:48:38 +0700

    test 1

--- src/shared/utils/token.ts
+++ src/shared/utils/token.ts
@@ -3,14 +3,12 @@ import { JWTUser } from 'src/modules/auth/auth.type';
 import { v4 as uuidv4 } from 'uuid';
 
 export function generateAccessToken(user: JWTUser) {
+  console.log('test 1');
   const accessToken = sign({ ...user }, process.env.ACCESS_TOKEN_SECRET_KEY);
   return accessToken;
 }
 
 export function generateRefreshToken() {
   const token = uuidv4();
-  console.log('token 1');
-  console.log('token 2 sua lai');
-  console.log('feat 1');
   return token;
 }
